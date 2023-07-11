4c8fbb20799131a4eb42e72d0d2e0a50cfd69236
Taqi haider <taqihaider591@gmail.com>
2023-07-11 22:49:44 +0500

    push

---
+++ P7.cpp
@@ -0,0 +1,15 @@
+// Print the sum, difference and product of two complex numbers by creating a class named 'Complex'
+//  with separate functions for each operation whose real and imaginary parts are entered by the user.
+#include<iostream>
+using namespace std;
+class Complex{
+    private:
+    int real,img;
+    public:
+    Complex(int r,int i):real(r),img(i){}
+    
+};
+int main(){
+    
+    return 0;
+}
