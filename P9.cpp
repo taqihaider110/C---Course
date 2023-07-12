38a3e981907191817f607b2bd72f0f88c2c50d9f
Taqi haider <taqihaider591@gmail.com>
2023-07-13 00:47:44 +0500

    push

--- P9.cpp
+++ P9.cpp
@@ -11,19 +11,19 @@ class Employee{
     string name,yearofjoining,address;
     double salary;
     public:
-    Employee():name(""),yearofjoining(""),salary(0.0),address(""){
-        cout<<"Enter Data for user:"<<count<<endl;
-        cout<<"Enter Name:";
-        getline(cin>>ws,name);
-        cout<<"Enter Year of Joining:";
-        getline(cin>>ws,yearofjoining);
-        cout<<"Enter Salary :";
-        cin>>salary;
-        cout<<"Enter Address :";
-        getline(cin>>ws,address);
-        count++;
-    }
-    // Employee(string n,string y,double s,string add):name(n),yearofjoining(y),salary(s),address(add){
+    // Employee():name(""),yearofjoining(""),salary(0.0),address(""){
+    //     cout<<"Enter Name:";
+    //     getline(cin>>ws,name);
+    //     cout<<"Enter Year of Joining:";
+    //     getline(cin>>ws,yearofjoining);
+    //     cout<<"Enter Salary :";
+    //     cin>>salary;
+    //     cout<<"Enter Address :";
+    //     getline(cin>>ws,address);
+    //     count++;
+    // }
+    Employee() : name(""), yearofjoining(""), salary(0.0), address("") {}
+    Employee(string n,string y,double s,string add):name(n),yearofjoining(y),salary(s),address(add){
     //     cout<<"Enter Name:";
     //     cin>>name;
     //     cout<<"Enter Year of Joining:";
@@ -32,27 +32,46 @@ class Employee{
     //     cin>>salary;
     //     cout<<"Enter Address :";
     //     cin>>address;
-    // }
+    }
+    void setData(){
+        cout << "Enter Name: ";
+        getline(cin >> ws, name);
+
+        cout << "Enter Year of Joining: ";
+        getline(cin >> ws, yearofjoining);
+
+        cout << "Enter Salary: ";
+        cin >> salary;
+
+        cout << "Enter Address: ";
+        getline(cin >> ws, address);
+    }
+
     void display(){
         // cout<<"The Name of Employee is :"<<name<<"\t\t";
-        cout<<name<<"\t\t";
+        cout<<name<<"\t\t"<<yearofjoining<<"\t\t\t"<<salary<<"\t\t\t"<<address<<"\t\t\t"<<endl;
         // cout<<"The Year of Joining of Employee is :"<<yearofjoining<<"\t\t";
-        cout<<yearofjoining<<"\t\t\t";
         // cout<<"The Salary of Employee is :"<<salary<<"\t\t";
-        cout<<salary<<"\t\t\t";
         // cout<<"The Address of Employee is :"<<address<<"\t\t";
-        cout<<address<<"\t\t";
-        cout<<endl;
     }
 };  
 int main(){
-    Employee e1,e2,e3;
-    cout<<"Name\t\t"<<"Year of joining\t\t"<<"Salary\t\t"<<"Address\t\t"<<endl;
+    const int numEmployees = 3;
+    Employee employees[numEmployees];
+    // Employee e1,e2,e3;
+    for (int i = 0; i < numEmployees; i++) {
+        cout << "Enter Data for user " << (i + 1) << endl;
+        employees[i].setData();
+    }
+    cout<<"Name\t\tYear of joining\t\tSalary\t\tAddress\t\t"<<endl;
+    for (int i = 0; i < numEmployees; i++) {
+        employees[i].display();
+    }
     // Employee e1("Taqi","2026",185000,"D-14,Ancholi");
-    e1.display();
+    // e1.display();
     // Employee e2("Rehan","2026",250000,"Chappal Garden");
-    e2.display();
+    // e2.display();
     // Employee e3("Murti","2026",200000,"North Nazimabad");
-    e3.display();
+    // e3.display();
     return 0;
 }
