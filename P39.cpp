007e35f93470ec36d8eece645b5e30f8a73437d3
Taqi haider <taqihaider591@gmail.com>
2023-07-30 02:14:40 +0500

    Abstract class and Virtual Function

---
+++ P39.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Shape{
+protected:
+float dimension;
+public:
+void getDimension( ){
+cin >> dimension;
+}
+private:
+// pure virtual Function
+virtual float calculateArea() = 0;
+};
+// Derived class
+class Square : public Shape{
+public:
+float calculateArea( ){
+return dimension * dimension;
+}
+};
+// Derived class
+class Circle : public Shape{
+public:
+float calculateArea( ) {
+return 3.14 * dimension * dimension;
+}
+};
+int main( ){
+Square square;
+Circle circle;
+cout << "Enter the length of the square: ";
+square.getDimension( );
+cout << "Area of square: " << square.calculateArea( ) << endl;
+cout << "\nEnter radius of the circle: ";
+circle.getDimension( );
+cout << "Area of circle: " << circle.calculateArea( ) << endl;
+return 0;
+}
