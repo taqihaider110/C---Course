9de787e4ac4008d85243712227b4a2b369482a82
Taqi haider <taqihaider591@gmail.com>
2023-07-30 00:59:57 +0500

    Function Overriding

---
+++ P33.cpp
@@ -0,0 +1,26 @@
+// Function Overriding
+#include<iostream>
+using namespace std;
+class BaseClass{
+public:
+virtual void Display( ){
+cout << "\nThis is Display( ) method of BaseClass";
+}
+void Show( ){
+cout << "\nThis is Show() method of BaseClass";
+}
+};
+class DerivedClass : public BaseClass {
+public:
+// Overriding method - new working of base class display method
+void Display( ){
+cout << "\nThis is Display( ) method of DerivedClass";
+}
+};
+// Driver code
+int main( ){
+DerivedClass dr;
+BaseClass &bs = dr;
+bs.Display( );
+dr.Show( );
+}
