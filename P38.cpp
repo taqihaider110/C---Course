cc3a9c730cffa0323f3a748c8bad4c0ee1fa4c91
Taqi haider <taqihaider591@gmail.com>
2023-07-30 01:59:51 +0500

    Virtual Base class

---
+++ P38.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class PoweredDevice
+{
+public:
+    PoweredDevice(int power)
+    {
+        cout << "PoweredDevice: " << power << '\n';
+    }
+};
+class Scanner : virtual public PoweredDevice
+{
+public:
+    Scanner(int scanner, int power) : PoweredDevice(power)
+    {
+        cout << "Scanner: " << scanner << '\n';
+    }
+};
+class Printer : virtual public PoweredDevice
+{
+public:
+    Printer(int printer, int power) : PoweredDevice(power)
+    {
+        cout << "Printer: " << printer << '\n';
+    }
+};
+class Copier : public Scanner, public Printer
+{
+public:
+    Copier(int scanner, int printer, int power) : PoweredDevice(power),
+                                                  Scanner(scanner, power), Printer(printer, power) {}
+};
+int main()
+{
+    Copier copier(1, 2, 3);
+    return 0;
+}
