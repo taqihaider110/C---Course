650a48b785a99b2cb17b591fd1768f2dd3c23238
Taqi haider <taqihaider591@gmail.com>
2023-09-20 03:18:00 +0500

    push

--- P62.cpp
+++ P62.cpp
@@ -1,3 +1,21 @@
 // Question: Design a C++ class Math with a static method square that 
 // calculates the square of an integer. Create a client program 
 // that uses this static method to find the squares of multiple numbers.
+#include <iostream>
+
+class Math {
+public:
+    // Static method to calculate the square of an integer
+    static int square(int x) {
+        return x * x;
+    }
+};
+
+int main() {
+    int num = 5;
+    int squared = Math::square(num); // Calling the static method using the class name
+
+    std::cout << "The square of " << num << " is: " << squared << std::endl;
+
+    return 0;
+}
